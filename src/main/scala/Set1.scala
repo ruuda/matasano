8d8c3ee8f2cbb51bb6408a1c3da8104d9acf39eb
Ruud van Asseldonk <dev@veniogames.com>
2016-05-03 15:13:31 +0200

    Implement decodeHex

--- src/main/scala/Set1.scala
+++ src/main/scala/Set1.scala
@@ -5,7 +5,27 @@
 // A copy of the License is available in the root of the repository.
 
 object Challenge1 {
-  def decodeHex(str: String): Array[Byte] = ???
+  val hexChars = "0123456789abcdef"
+  val base64Chars = ???
+
+  def decodeHex(str: String): Array[Byte] = {
+    if (str.length == 0) {
+      Array()
+    } else if (str.length >= 2) {
+      val byteStr = str.take(2)
+      val remainder = str.drop(2)
+      val high = hexChars.indexOf(byteStr(0))
+      val low = hexChars.indexOf(byteStr(1))
+      if (high == -1 || low == -1) {
+        throw new Exception(s"Invalid input, '$byteStr' is not hexadecimal.")
+      }
+      val byte = ((high << 4) | low).toByte
+      Array(byte) ++ decodeHex(remainder)
+    } else {
+      throw new Exception("Invalid input, not an integer number of bytes.")
+    }
+  }
+
   def encodeHex(data: Array[Byte]): String = ???
   def decodeBase64(str: String): Array[Byte] = ???
   def encodeBase64(data: Array[Byte]): String = ???
