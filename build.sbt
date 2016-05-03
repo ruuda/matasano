d558cae0640c63c5b3dce37c608629bacb285c79
Ruud van Asseldonk <dev@veniogames.com>
2016-05-03 16:53:53 +0200

    Add test for challenge 1

---
+++ build.sbt
@@ -0,0 +1,2 @@
+name := "matasano"
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.13.0" % "test"
---
+++ src/test/scala/Set1.scala
@@ -0,0 +1,18 @@
+// Copyright 2016 Ruud van Asseldonk
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// A copy of the License is available in the root of the repository.
+
+import org.scalacheck.Properties
+import Challenge1._
+
+object Challenge1Spec extends Properties("Challenge1") {
+  property("spec") = {
+    val input = "49276d206b696c6c696e6720796f757220627261696e206c" +
+                "696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    val output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29" +
+                 "ub3VzIG11c2hyb29t"
+    output == encodeBase64(decodeHex(input))
+  }
+}
