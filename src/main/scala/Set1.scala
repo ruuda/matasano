0c0faedebaa2a265c180f9813c1d19f37b220cc6
Ruud van Asseldonk <dev@veniogames.com>
2016-05-03 15:25:25 +0200

    Simplify decodeHex

--- src/main/scala/Set1.scala
+++ src/main/scala/Set1.scala
@@ -8,23 +8,21 @@ object Challenge1 {
   val hexChars = "0123456789abcdef"
   val base64Chars = ???
 
-  def decodeHex(str: String): Array[Byte] = {
-    if (str.length == 0) {
-      Array()
-    } else if (str.length >= 2) {
-      val byteStr = str.take(2)
-      val remainder = str.drop(2)
-      val high = hexChars.indexOf(byteStr(0))
-      val low = hexChars.indexOf(byteStr(1))
-      if (high == -1 || low == -1) {
-        throw new Exception(s"Invalid input, '$byteStr' is not hexadecimal.")
-      }
-      val byte = ((high << 4) | low).toByte
-      Array(byte) ++ decodeHex(remainder)
-    } else {
+  def decodeByte(str: String): Byte = {
+    if (str.length != 2) {
       throw new Exception("Invalid input, not an integer number of bytes.")
     }
+    val high = hexChars.indexOf(str(0))
+    val low = hexChars.indexOf(str(1))
+    if (high == -1 || low == -1) {
+      throw new Exception(s"Invalid input, '$str' is not hexadecimal.")
+    } else {
+      ((high << 4) | low).toByte
+    }
   }
+
+  def decodeHex(str: String): Array[Byte] =
+    str.grouped(2).map(b => decodeByte(b)).toArray
 
   def encodeHex(data: Array[Byte]): String = ???
   def decodeBase64(str: String): Array[Byte] = ???
