39d5355a6bc496de7a00bb654cb90c73ab15647f
Ruud van Asseldonk <dev@veniogames.com>
2016-05-08 15:25:44 +0200

    Add a main function for challenge 2
    For consistency, let's add a main for every challenge that outputs a
    solution to the challenge.

--- src/main/scala/Set1/Challenge2.scala
+++ src/main/scala/Set1/Challenge2.scala
@@ -4,5 +4,15 @@
 // you may not use this file except in compliance with the License.
 // A copy of the License is available in the root of the repository.
 
+import com.matasano.Crypto
+import com.matasano.Encoding
+
 object Challenge2 {
+  def main(args: Array[String]) = {
+    val inputA = Encoding.decodeHex("1c0111001f010100061a024b53535009181c")
+    val inputB = Encoding.decodeHex("686974207468652062756c6c277320657965")
+    val data = Crypto.xor(inputA, inputB)
+    val output = Encoding.encodeHex(data)
+    println(output)
+  }
 }
