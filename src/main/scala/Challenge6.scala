0a96d25e63c5c206f01588d114937a6ade72fd2c
Ruud van Asseldonk <dev@veniogames.com>
2016-05-10 16:16:43 +0200

    Guess key size for repeated xor encryption

--- src/main/scala/Challenge6.scala
+++ src/main/scala/Challenge6.scala
@@ -7,11 +7,37 @@
 import io.Source
 
 import com.matasano.Encoding
+import com.matasano.Util
 
 object Challenge6 {
+  /// Returns a list of key size guesses, from most likely to least likely.
+  def guessKeySize(ciphertext: Vector[Byte], minSize: Int, maxSize: Int): Vector[Int] = {
+    val rankedSizes = (minSize to maxSize).map(keySize => {
+      // Inspect the first ten blocks for this key size.
+      val groups = ciphertext.grouped(keySize).take(10).toVector
+      val distances = for {
+        i <- 0 to groups.length - 1
+        j <- i + 1 to groups.length - 1
+      } yield Util.hammingDistance(groups(i), groups(j))
+
+      // Compute average number of different bits per byte.
+      val numGroups = groups.length * (groups.length - 1) / 2
+      var meanDistance = distances.sum.toFloat / numGroups.toFloat
+      var normDistance = meanDistance / keySize.toFloat
+
+      (normDistance, keySize)
+    })
+
+    rankedSizes.sorted.map { case (d, sz) => sz } .toVector
+  }
+
   def main(args: Array[String]) = {
     val file = Source.fromFile("data/challenge6.md")
     val ciphertext = Encoding.decodeBase64(file.getLines.mkString)
+
+    val keySize = guessKeySize(ciphertext, 2, 40).head
+    println(s"key size guess: $keySize")
+
     file.close()
   }
 }
