82bca346894bd9d65652506cf3cf259ccd332929
Ruud van Asseldonk <dev@veniogames.com>
2016-05-12 23:24:57 +0200

    Guess key based on histogram ranking

--- src/main/scala/Set1/Challenge6.scala
+++ src/main/scala/Set1/Challenge6.scala
@@ -6,7 +6,9 @@
 
 import io.Source
 
+import com.matasano.Crypto
 import com.matasano.Encoding
+import com.matasano.TextDetection
 import com.matasano.Util
 
 object Challenge6 {
@@ -31,12 +33,34 @@ object Challenge6 {
     rankedSizes.sorted.map { case (d, sz) => sz } .toVector
   }
 
+  def guessKey(ciphertext: Vector[Byte], keySize: Int): Vector[Byte] = {
+    val rankHist = TextDetection.buildHistogramRanker("data/frequency.md")
+    Vector.tabulate(keySize - 1) { i => {
+      val cipherChars = ciphertext.grouped(keySize)
+                                  .filter(block => block.length > i)
+                                  .map(block => block(i))
+                                  .toVector
+      val keys = Vector.tabulate(255) { key => {
+        val plaintext = Crypto.xorSingle(cipherChars, key.toByte)
+        val histogram = TextDetection.buildHistogram(plaintext.map(byte => byte.toChar).to)
+        val rank = rankHist(histogram)
+        (key, rank)
+      }}
+      val (bestKey, _) = keys.maxBy { case (key, rank) => rank }
+      bestKey.toByte
+    }}
+  }
+
   def main(args: Array[String]) = {
     val file = Source.fromFile("data/challenge6.md")
     val ciphertext = Encoding.decodeBase64(file.getLines.mkString)
 
     val keySize = guessKeySize(ciphertext, 2, 40).head
     println(s"key size guess: $keySize")
+
+    val key = guessKey(ciphertext, keySize)
+    val keyStr = Encoding.decodeAscii(key)
+    println(s"key guess: $keyStr")
 
     file.close()
   }
