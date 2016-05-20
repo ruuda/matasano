94cf5bb5d8e1ba901ec408547be87db4e6f7887e
Ruud van Asseldonk <dev@veniogames.com>
2016-05-20 10:08:53 +0200

    Wrap AES block cipher from framework

--- src/main/scala/Aes.scala
+++ src/main/scala/Aes.scala
@@ -7,11 +7,23 @@
 package com.matasano
 
 object Aes {
+  import javax.crypto.Cipher
+  import javax.crypto.spec.SecretKeySpec
+
   def encrypt(block: Vector[Byte], key: Vector[Byte]): Vector[Byte] = {
-    ???
+    // Use the Java AES implementation only for its block cipher.
+    // The point of this exercise it to learn how to do things myself,
+    // so I will not use the standard block-chaining or padding mechanisms.
+    val cipher = Cipher.getInstance("AES/ECB/NoPadding")
+    val keySpec = new SecretKeySpec(key.toArray, "AES")
+    cipher.init(Cipher.ENCRYPT_MODE, keySpec)
+    cipher.doFinal(block.toArray).toVector
   }
 
   def decrypt(block: Vector[Byte], key: Vector[Byte]): Vector[Byte] = {
-    ???
+    val cipher = Cipher.getInstance("AES/ECB/NoPadding")
+    val keySpec = new SecretKeySpec(key.toArray, "AES")
+    cipher.init(Cipher.DECRYPT_MODE, keySpec)
+    cipher.doFinal(block.toArray).toVector
   }
 }
