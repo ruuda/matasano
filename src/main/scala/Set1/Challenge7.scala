0101e06451d04cc7b00d301107822dc93403e7bc
Ruud van Asseldonk <dev@veniogames.com>
2016-05-22 23:58:46 +0200

    Solve challenge 7
    There are some weird trailing characters though, do I have to deal with
    padding here? Let's not bother until something like this comes up again.

---
+++ src/main/scala/Set1/Challenge7.scala
@@ -0,0 +1,27 @@
+// Copyright 2016 Ruud van Asseldonk
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// A copy of the License is available in the root of the repository.
+
+import io.Source
+
+import com.matasano.Aes
+import com.matasano.Encoding
+
+object Challenge7 {
+  def main(args: Array[String]) = {
+    val file = Source.fromFile("data/challenge7.md")
+    val ciphertext = Encoding.decodeBase64(file.getLines.mkString)
+    val key = Encoding.encodeAscii("YELLOW SUBMARINE")
+    val plaintext = ciphertext
+      .grouped(16)
+      .map(Aes.decrypt(key, _))
+      .map(Encoding.decodeAscii)
+      .mkString
+
+    println(s"plaintext:\n\n$plaintext")
+
+    file.close()
+  }
+}
